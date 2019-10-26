c8f939bc64fa1b54d0b339670be5ffd8d651183c
Tarik02 <Taras.Fomin@gmail.com>
2019-10-26 10:43:59 +0300

    Add КБ-11 curriculum

--- src/curriculum.ts
+++ src/curriculum.ts
@@ -223,6 +223,94 @@ export const curriculums: Record<string, Curriculum> = {
 			],
 		],
 	},
+
+	'КБ-11': {
+		'Tu': [
+			{
+				subject: 'Дискретна матем.',
+				room: 'Л.411'
+			},
+			{
+				subject: 'Фіз.виховання'
+			},
+			{
+				subject: 'Програмування',
+				room: 'Л.1'
+			},
+			[
+				{
+					subject: 'Історія держ.',
+					room: 'Л.3'
+				},
+				{
+					subject: 'Дискр.матем.',
+					room: 'Л.303'
+				},
+			],
+		],
+		'We': [
+			{
+				subject: 'Комп. графіка',
+				room: 'Л.400',
+			},
+			[
+				{
+					subject: 'Комп. графіка',
+					room: 'Л.400',
+				},
+				{
+					subject: 'Дискр.матем.',
+					room: 'Л.301',
+				},
+			],
+			{
+				subject: 'Дискр. матем.',
+				room: 'Л.301',
+			},
+			{
+				subject: 'Програмування',
+				room: 'Л.39',
+			},
+		],
+		'Th': [
+			{
+				subject: 'Історія укр.держ.',
+				room: 'Л.153',
+			},
+			{
+				subject: 'Фіз.виховання'
+			},
+		],
+		'Fr': [
+			undefined,
+			{
+				subject: 'Вища.матем.',
+				room: 'Л.304',
+			},
+			{
+				subject: 'Вища.матем.',
+				room: 'Л.304',
+			},
+			{
+				subject: 'Іноземна мова',
+				room: 'Л.241',
+			},
+		],
+		'Sa': [
+			{
+				subject: 'Осн.акад.письма',
+				room: 'Л.153',
+			},
+			{
+				subject: 'Дискр.матем.',
+				room: 'Л.313',
+			},
+			{
+				subject: 'Програмування',
+				room: 'Л.303a',
+			},
+		],
+	},
 };
 
 export const groups = Object.keys(curriculums);
