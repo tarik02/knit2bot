bfdd2fb99cf252f23ea6854a21509838e420e01b
Tarik02 <Taras.Fomin@gmail.com>
2020-02-25 21:48:35 +0200

    Update КСМ-11 curriculum

--- src/curriculums/КСМ-11.ts
+++ src/curriculums/КСМ-11.ts
@@ -3,89 +3,77 @@ import { Curriculum } from '../curriculum';
 const curriculum: Curriculum = {
 	'Tu': [
 		{
-			subject: 'Дискретна матем.',
-			room: 'Л.411'
-		},
-		{
-			subject: 'Фіз.виховання'
-		},
-		{
 			subject: 'Програмування',
-			room: 'Л.1'
+			room: 'Л.303'
 		},
-		[
-			{
-				subject: 'Історія держ.',
-				room: 'Л.3'
-			},
-			{
-				subject: 'Дискр.матем.',
-				room: 'Л.303'
-			},
-		],
+		{
+			subject: 'Фіз. Виховання'
+		},
 	],
 	'We': [
 		{
-			subject: 'Комп. графіка',
-			room: 'Л.400',
+			subject: 'Вища математика',
+			room: 'Л.404',
 		},
 		[
 			{
-				subject: 'Комп. графіка',
-				room: 'Л.400',
+				subject: 'Укр. мова',
+				room: 'Л.1',
 			},
 			{
-				subject: 'Дискр.матем.',
-				room: 'Л.301',
+				subject: 'Філософія',
+				room: 'Л.36',
 			},
 		],
-		[
-			{
-				subject: 'Програмування',
-				room: 'Л.303а',
-			},
-			undefined,
-		],
+		{
+			subject: 'Вища математика',
+			room: 'Л.2',
+		},
+		{
+			subject: 'Іноземна мова (нім.)',
+			room: 'Л.241'
+		},
 	],
 	'Th': [
+		undefined,
 		{
-			subject: 'Історія укр.держ.',
-			room: 'Л.153',
+			subject: 'Філософія',
+			room: 'Л.324',
 		},
 		{
-			subject: 'Фіз.виховання'
-		},
-		{
-			subject: 'Програмування',
-			room: 'Л.303а',
+			subject: 'Укр. мова',
+			room: 'Л.231',
 		},
 	],
 	'Fr': [
+		[
+			undefined,
+			{
+				subject: 'Програмування',
+				room: 'Л.303',
+			},
+		],
 		{
-			subject: 'Іноземна мова',
-			room: 'Л.255',
+			subject: 'Програмування',
+			room: 'Л.303',
 		},
 		{
-			subject: 'Дискр.матем.',
-			room: 'Л.301',
+			subject: 'Фізика',
+			room: 'Л.317',
 		},
 		{
-			subject: 'Вища.матем.',
-			room: 'Л.304',
+			subject: 'Фізика',
+			room: 'Л.322',
 		},
 	],
 	'Sa': [
 		{
-			subject: 'Осн.акад.письма',
-			room: 'Л.153',
+			subject: 'Іноземна мова',
+			room: 'Л.241',
 		},
 		{
-			subject: 'Дискр.матем.',
-			room: 'Л.313',
-		},
-		{
-			subject: 'Вища.матем.',
-			room: 'Л.304',
+			subject: 'Програмування',
+			room: 'Л.303',
 		},
 	],
 };
